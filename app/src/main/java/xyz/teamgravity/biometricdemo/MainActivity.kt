2ae287b73234f015d12bdecb672922807f314e07
raheemadamboev <vividricemaryandmax@gmail.com>
2024-03-26 20:38:43 +0500

    Initial;

---
+++ app/src/main/java/xyz/teamgravity/biometricdemo/MainActivity.kt
@@ -0,0 +1,28 @@
+package xyz.teamgravity.biometricdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import xyz.teamgravity.biometricdemo.ui.theme.BiometricDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BiometricDemoTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
